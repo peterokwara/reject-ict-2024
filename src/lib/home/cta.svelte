5351dc92b71f6d0f68c79cbefd65c649a97b776c
Peter Okwara <ojakaapeter@gmail.com>
2024-07-09 20:41:17 +0300

    more clearer

--- src/lib/home/cta.svelte
+++ src/lib/home/cta.svelte
@@ -13,7 +13,7 @@
 		<p class="text-3xl lg:text-4xl">👇</p>
 
 		<div class="grid w-full max-w-4xl grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-			<a href="/submit" class="w-full text-white btn btn-primary"> Create Your Submission </a>
+			<a href="/submit" class="w-full text-white btn btn-primary"> Start Here </a>
 			<a
 				href="https://twitter.com/intent/tweet?text=Join+us+in+rejecting+the+ICTBill2024+%23RejectICTBill2024+%23RejectICTBill&url=https%3A%2F%2Freject-ict-2024.vercel.app%2F"
 				class="w-full text-white btn btn-secondary"
@@ -33,37 +33,37 @@
 				href="https://x.com/0xNdeto/status/1810644078024962511"
 				class="w-full text-white btn btn-secondary"
 			>
-				Follow the discussion
+				Join the Discussion
 			</a>
 			<a
 				href="https://cms.icta.go.ke//sites/default/files/2024-06/ICTA%20BILL%20FINAL%20FOR%20PUBLICATION.pdf"
 				class="w-full text-white btn btn-secondary"
 			>
-				Read the bill
+				Read the Bill
 			</a>
 			<a
 				href="https://chatgpt.com/g/g-onRXZwI9D-ict-authority-bill-2024-gpt"
 				class="w-full text-white btn btn-secondary"
 			>
-				Chat with bill (1)
+				Talk to the Bill using Chatgpt (1)
 			</a>
 			<a
 				href="https://chatgpt.com/g/g-eLeBzwkRa-proposed-ict-bill-paralegal"
 				class="w-full text-white btn btn-secondary"
 			>
-				Chat with bill (2)
+				Talk to the Bill using Chatgpt (2)
 			</a>
 			<a
 				href="https://github.com/peterokwara/reject-ict-2024"
 				class="w-full text-white btn btn-secondary"
 			>
-				View GitHub repo
+				Explore the GitHub
 			</a>
 			<a
 				href="https://docs.google.com/document/d/e/2PACX-1vSl7n1Fp934pS1EpMBQkETb3HiBFpLg2Lyaja6z8PEsviVin0qgxM8lT47KXYowqYpFeEHfXo6N6EMH/pub"
 				class="w-full text-white btn btn-secondary"
 			>
-				Email Template
+				Use the Email Template
 			</a>
 		</div>
 	</div>
